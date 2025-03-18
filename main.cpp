0c54fc5cd13ce579a64f067ef22b8993f9ffa1da
GalPovsod8 <galpos10@gmail.com>
2025-03-18 13:19:22 +0100

    preapeared file writing functionality

--- main.cpp
+++ main.cpp
@@ -21,6 +21,17 @@ vector<unsigned char> readInputFile(const string &filename) {
     return numbers;
 }
 
+void writeOutputFile(const string &filename, const vector<unsigned char> &numbers) {
+    ofstream file(filename);
+    if (file.is_open()) {
+        for (size_t i = 0; i < numbers.size(); i++) {
+            if (i > 0) file << " ";
+            file << static_cast<int>(numbers[i]);
+        }
+        file.close();
+    }
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         cerr << "Uporaba: " << argv[0] << " <vhodna_datoteka>" << endl;
