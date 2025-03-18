6838808aec95717e4d8ed90b336d58a0c520a154
GalPovsod8 <galpos10@gmail.com>
2025-03-18 13:18:18 +0100

    added file reading functionality

--- main.cpp
+++ main.cpp
@@ -5,6 +5,22 @@
 
 using namespace std;
 
+vector<unsigned char> readInputFile(const string &filename) {
+    ifstream file(filename);
+    vector<unsigned char> numbers;
+    string line, temp;
+    
+    if (file.is_open()) {
+        getline(file, line);
+        file.close();
+        stringstream ss(line);
+        int num;
+        while (ss >> num)
+            numbers.push_back(static_cast<unsigned char>(num));
+    }
+    return numbers;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         cerr << "Uporaba: " << argv[0] << " <vhodna_datoteka>" << endl;
@@ -13,6 +29,8 @@ int main(int argc, char *argv[]) {
     
     string inputFile = argv[1];
     string outputFile = "out.txt";
+
+    vector<unsigned char> numbers = readInputFile(inputFile);
     
     return 0;
 }
