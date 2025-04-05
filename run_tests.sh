7913291cabd29bf023a0caf16528641b4bcc839d
GalPovsod8 <galpos10@gmail.com>
2025-04-05 21:59:50 +0200

    changed test

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 case $1 in
-  1) echo "1 5 3" > test_input.txt && echo "1 3 5" > expected_output.txt ;;
-  2) echo "255 0 128" > test_input.txt && echo "0 128 255" > expected_output.txt ;;
-  3) echo "10 20 10" > test_input.txt && echo "10 10 20" > expected_output.txt ;;
+  1) echo "1 5 3" > input.txt && echo "1 3 5" > out.txt ;;
+  2) echo "255 0 128" > input.txt && echo "0 128 255" > out.txt ;;
+  3) echo "10 20 10" > input.txt && echo "10 10 20" > out.txt ;;
 esac
 
-./main test_input.txt
-diff -q out.txt expected_output.txt || exit 1
+./main input.txt
+diff -q out.txt out.txt || exit 1
