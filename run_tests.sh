b1cadd3a325cf80cee28f7a332b441b1dd7bd5ae
GalPovsod8 <galpos10@gmail.com>
2025-04-06 20:01:51 +0200

    updated test

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 case $1 in
-  1) echo "1 5 3" > input.txt && echo "1 3 5" > out.txt ;;
-  2) echo "255 0 128" > input.txt && echo "0 128 255" > out.txt ;;
-  3) echo "10 20 10" > input.txt && echo "10 10 20" > out.txt ;;
+  1) echo "1 5 3" > input.txt && echo "1 3 5" > expected.txt ;;
+  2) echo "255 0 128" > input.txt && echo "0 128 255" > expected.txt ;;
+  3) echo "10 20 10" > input.txt && echo "10 10 20" > expected.txt ;;
+  *) echo "Neveljaven test case!" && exit 1 ;;
 esac
 
 ./dn1 input.txt
-diff -q out.txt out.txt || exit 1
+diff -q out.txt expected.txt || exit 1
